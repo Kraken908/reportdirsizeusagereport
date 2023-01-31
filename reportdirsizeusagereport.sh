e4adee0ce0e8a7acd0874253a9b63c5f49d4fddb
kraken <70107358+Kraken908@users.noreply.github.com>
2023-01-31 15:53:07 -0700

    Update reportdirsizeusagereport.sh

--- reportdirsizeusagereport.sh
+++ reportdirsizeusagereport.sh
@@ -12,3 +12,19 @@ else
     echo "Error: no report generated.  $dir not accessible" >> /tmp/report
 fi
 #cat the report file in /tmp folder named report
+#below this script are variations you can use to find the disk usage of file directories 
+# just remove the hash tags and copy and paste it to a new script file  
+
+##!/bin/bash
+ 
+#read -p "Enter a directory: " dir
+#start=$(date)
+#echo "Document directory usage report" > /tmp/report
+#if du -sh $dir >> /tmp/report 2> /dev/null
+#then
+#   echo "Start of report: $start" >> /tmp/report
+#   echo "End of report: $(date)" >> /tmp/report
+#else
+#   echo "Error, $dir could not be accessed"
+#   echo "Error: no report generated.  $dir not accessible" >> /tmp/report
+#fi
